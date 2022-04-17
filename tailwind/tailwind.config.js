4307fefa6fbad81cd1d7938e8d7faa3e12a54207
Zac <a19860101@gmail.com>
2022-04-17 15:05:28 +0800

    20220417

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -2,9 +2,15 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     fontFamily:{
-      sans: ['PT sans','lihsianti','Noto Sans TC'],
+      sans: ['PT sans','Noto Sans TC'],
       serif: ['georgia'],
     } , 
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px'
+    },
     extend: {
       colors:{
         primary: '#42C2FF',
