b70f6a6cb1a74b0ca35de1f8dc200bcb7f827261
Zac <a19860101@gmail.com>
2022-06-26 10:33:09 +0800

    20220626

---
+++ vue/js/AppButton.js
@@ -0,0 +1,12 @@
+export default {
+    template: `
+        <button :disabled="isDisabled">
+            <slot />
+        </button>
+    `,
+    data(){
+        return {
+            isDisabled: false
+        }
+    }
+}
