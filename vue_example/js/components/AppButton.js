d1c643475f38557079c753bc60ae1ac3e14cc67a
Zac <a19860101@gmail.com>
2022-06-26 12:04:27 +0800

    20220626

--- vue_example/js/components/AppButton.js
+++ vue_example/js/components/AppButton.js
@@ -1,9 +1,16 @@
 export default {
-    props:['btnStyle'],
+    // props:['btnStyle'],
+    props:{
+        btnStyle:{
+            default: 'muted',
+            type:String
+        }
+    },
     template:`
         <button 
             :class="{
                 'p-3 text-white rounded-md m-3': true,
+                'bg-zinc-300': btnStyle === 'muted',
                 'bg-blue-600': btnStyle === 'primary',
                 'bg-red-600': btnStyle === 'danger',
                 'bg-green-600': btnStyle === 'success',
