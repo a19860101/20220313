0113b2d3a2c3ef31aa7abbbf01b4804fc0b6d0f8
Zac <a19860101@gmail.com>
2022-07-03 15:00:16 +0800

    20220703

---
+++ vite-practice/vite-1/src/components/About.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+<template>
+    <h1>About</h1>
+</template>
+<style scoped>
+    h1 {
+        color: red;
+    }
+</style>
---
+++ vite-practice/vite-1/src/components/Test.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Test</h1>
+</template>
+<style scoped>
+h1 {
+    color: orange;
+  }
+</style>
