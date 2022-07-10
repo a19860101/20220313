1c8322674b07aeade5b9f84aa6639a34904de4e7
Zac <a19860101@gmail.com>
2022-07-10 12:06:46 +0800

    20220710

--- vite-practice/vite-2/src/components/Nav.vue
+++ vite-practice/vite-2/src/components/Nav.vue
@@ -4,7 +4,16 @@
       <router-link to="/about">About</router-link>
       <router-link to="/service">Service</router-link>
       <router-link to="/contact">Contact</router-link>
-      <router-link to="/product">Product</router-link>
+      <router-link to="/product/123?test=banana">Product</router-link>
+      <router-link :to="{
+        name: product,
+        params:{
+            id: 9527
+        },
+        query:{
+            test: '0998098',
+        }
+      }">Product</router-link>
     </nav>
 </template>
 <style scoped>
--- vite-practice/vite-2/src/views/Product.vue
+++ vite-practice/vite-2/src/views/Product.vue
@@ -2,5 +2,6 @@
     <div>
         <h1>Product</h1>
         {{$route.params.id}}
+        {{$route.query.test}}
     </div>
 </template>
