ac4ed081d797c6f8cae7cd1c232ae057a65ba261
Zac <a19860101@gmail.com>
2022-07-10 13:30:15 +0800

    20220710

---
+++ vite-practice/vite-2/src/components/Back.vue
@@ -0,0 +1,7 @@
+<template>
+<div>
+    <a href="#" @click="$router.back()">Back</a> |
+    <a href="#" @click="$router.go(1)">Next</a> |
+    <a href="#" @click="$router.go(-1)">Prev</a>
+</div>
+</template>
--- vite-practice/vite-2/src/views/About.vue
+++ vite-practice/vite-2/src/views/About.vue
@@ -1,3 +1,13 @@
+<script>
+    import Back from '@/components/Back.vue'
+    export default {
+        components: {Back}
+    }
+</script>
+
 <template>
+<div>
     <h1>About</h1>
+    <Back />
+</div>
 </template>
