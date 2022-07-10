5e64bf457aa9f3da5c6a95bb737b44fc8447eb7c
Zac <a19860101@gmail.com>
2022-07-10 09:55:58 +0800

    20220710

--- vite-practice/vite-2/src/App.vue
+++ vite-practice/vite-2/src/App.vue
@@ -2,16 +2,16 @@
 </script>
 
 <template>
-  <h1>My Vite App</h1>
+  <div>
+    <h1>My Vite App</h1>
+    <nav>
+      <router-link to="/">Home</router-link> | 
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view></router-view>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- vite-practice/vite-2/src/main.js
+++ vite-practice/vite-2/src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
-import {createRouter} from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import App from './App.vue'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 
 const router = createRouter({
+    history:createWebHistory(),
     routes:[
         {
             path: '/',
