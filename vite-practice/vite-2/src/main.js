443c8a6d91a6fb9222f8a8e0b89b4eb9e17b94be
Zac <a19860101@gmail.com>
2022-07-10 10:14:28 +0800

    20220710

--- vite-practice/vite-2/src/main.js
+++ vite-practice/vite-2/src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import {createRouter, createWebHistory} from 'vue-router'
 import App from './App.vue'
 import Home from './views/Home.vue'
-import About from './views/About.vue'
+// import About from './views/About.vue'
+// import Service from './views/Service.vue'
+// import Contact from './views/Contact.vue'
 
 const router = createRouter({
     history:createWebHistory(),
@@ -15,8 +17,22 @@ const router = createRouter({
         {
             path: '/about',
             name: 'about',
-            component: About
+            // component: About
+            component: () => import('./views/About.vue')
+        },
+        {
+            path: '/service',
+            name: 'service',
+            // component: Service
+            component: () => import('./views/Service.vue')
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            // component: Contact
+            component: () => import('./views/Contact.vue')
         }
+        
     ]
 });
 
