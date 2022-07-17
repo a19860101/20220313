f039622c2a4883cd0c735d645cb44aaf4ec2abd1
Zac <a19860101@gmail.com>
2022-07-17 14:16:30 +0800

    20220717

--- react/my-react/src/Counter.js
+++ react/my-react/src/Counter.js
@@ -1,12 +1,17 @@
 import {useState} from 'react'
 const Counter = ()=>{
     const [count, setCount] = useState(0);
+
+    const countMinus = () => setCount(data => data -= 1);
+    // const countMinus = () => {return setCount((data) => {return data -= 1})};
     
     return (
         <>
-            <button>+</button>
+            <button onClick={()=>{return setCount((data)=>{return data += 1})}}>+</button>
+            <button onClick={()=>setCount(data => data += 1)}>+</button>
             <h1>{count}</h1>
-            <button>-</button>
+            <button onClick={()=>setCount(data => data -= 1)}>-</button>
+            <button onClick={countMinus}>-</button>
         </>
     )
 }
