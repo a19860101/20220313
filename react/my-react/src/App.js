cf83c99962b5fb11bbf3758577d41e0f402ed105
Zac <a19860101@gmail.com>
2022-07-17 13:47:11 +0800

    20220717

--- react/my-react/src/App.js
+++ react/my-react/src/App.js
@@ -1,12 +1,12 @@
-
+import Test from './Test.js'
 
 function App() {
-  const title = 'Hello React!!!';
-  const num = ['apple','banana','cat'];
   return (
     <div className="App">
-      <h1>{title}</h1>
-      <div>{num}</div>
+      <Test name="john"/>
+      <Test name="george"/>
+      <Test name="mary"/>
+      <Test name="zoy"/>
     </div>
   );
 }
