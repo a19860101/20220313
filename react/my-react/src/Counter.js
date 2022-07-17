197522885a2a81983ff8d78d8acb097e7df12923
Zac <a19860101@gmail.com>
2022-07-17 14:34:10 +0800

    20220717

--- react/my-react/src/Counter.js
+++ react/my-react/src/Counter.js
@@ -1,16 +1,26 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 const Counter = ()=>{
     const [count, setCount] = useState(0);
 
-    const countMinus = () => setCount(data => data -= 1);
-    // const countMinus = () => {return setCount((data) => {return data -= 1})};
+    const countMinus = () => setCount(prevData => prevData -= 1);
+    // const countMinus = () => {return setCount((prevData) => {return prevData -= 1})};
     
+    useEffect(()=>{
+        // setCount(6);
+        console.log('useEffect',count);
+    },[count])
+    /* 
+        沒有任何參數: 每次變化皆會執行
+        空陣列: 只有初始化會執行
+        陣列內有指定變數: 陣列內變數有變化時執行
+    */
+
     return (
         <>
-            <button onClick={()=>{return setCount((data)=>{return data += 1})}}>+</button>
-            <button onClick={()=>setCount(data => data += 1)}>+</button>
+            <button onClick={()=>{return setCount((prevData)=>{return prevData += 1})}}>+</button>
+            <button onClick={()=>setCount(prevData => prevData += 1)}>+</button>
             <h1>{count}</h1>
-            <button onClick={()=>setCount(data => data -= 1)}>-</button>
+            <button onClick={()=>setCount(prevData => prevData -= 1)}>-</button>
             <button onClick={countMinus}>-</button>
         </>
     )
