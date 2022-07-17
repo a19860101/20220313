5a639ed0c765394deea4007a91a7d425e6d00c5f
Zac <a19860101@gmail.com>
2022-07-17 13:33:48 +0800

    20220717

---
+++ react/my-react/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  const title = 'Hello React!!!';
+  const num = ['apple','banana','cat'];
+  return (
+    <div className="App">
+      <h1>{title}</h1>
+      <div>{num}</div>
+    </div>
+  );
+}
+
+export default App;
